128b82db318189e12128d6203db4319c58b27625
kasra <montakhabikasra@gmail.com>
2025-09-17 16:18:24 +0300

    Add [k: string]: unknown to allow for additional properties in LoginResponse

--- types/auth.ts
+++ types/auth.ts
@@ -1,11 +1,12 @@
 export interface LoginResponse {
-  accessToken: string;
-  refreshToken: string;
-  id: number;
-  username: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  gender: string;
-  image: string;
+    accessToken: string;
+    refreshToken: string;
+    id: number;
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    image: string;
+    [k: string]: unknown;
 }
